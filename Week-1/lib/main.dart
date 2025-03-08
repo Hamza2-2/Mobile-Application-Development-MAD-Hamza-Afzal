64af7cd63aa2ec9aadcde5b6212817b580549a5c
Hamza2-2 <01-134222-060@student.bahria.edu.pk>
2025-03-08 06:12:35 +0500

    Week-1

--- Week-1/lib/main.dart
+++ Week-1/lib/main.dart
@@ -1,18 +1,74 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text(
+            'My Introduction',
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.blueAccent,
+          centerTitle: true,
+        ),
         body: Center(
-          child: Text('Hello World!'),
+          child: Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.star,
+                  color: Colors.yellow,
+                  size: 50,
+                ),
+                SizedBox(height: 10),
+                RichText(
+                  textAlign: TextAlign.center,
+                  text: TextSpan(
+                    style: TextStyle(fontSize: 18, color: Colors.black),
+                    children: [
+                      TextSpan(
+                        text: 'My name is ',
+                      ),
+                      TextSpan(
+                        text: 'Hamza Afzal',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      TextSpan(
+                        text:
+                            '. I am a passionate and dedicated computer science student. '
+                            'I possess ',
+                      ),
+                      TextSpan(
+                        text: 'strong problem-solving skills',
+                        style: TextStyle(fontStyle: FontStyle.italic),
+                      ),
+                      TextSpan(
+                        text:
+                            ' and a keen interest in technology. My qualities include being ',
+                      ),
+                      TextSpan(
+                        text: 'highly motivated, innovative, ',
+                        style: TextStyle(fontStyle: FontStyle.italic),
+                      ),
+                      TextSpan(
+                        text: 'and always eager to learn.',
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
