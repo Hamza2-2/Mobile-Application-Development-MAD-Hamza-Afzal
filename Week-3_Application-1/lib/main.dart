fd0cb93a257edf0151b148f0c47ff66c019cd921
Hamza2-2 <01-134222-060@student.bahria.edu.pk>
2025-03-22 09:45:26 +0500

    Updates added Week-3-Application-2

--- Week-3_Application-1/lib/main.dart
+++ Week-3_Application-1/lib/main.dart
@@ -2,49 +2,49 @@ import 'package:flutter/material.dart';
 import 'dart:math';
 
 void main() {
-  runApp(DiceRollerApp());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: DiceRollerApp(),
+  ));
 }
 
-class DiceRollerApp extends StatefulWidget {
-  @override
-  _DiceRollerAppState createState() => _DiceRollerAppState();
-}
+class DiceRollerApp extends StatelessWidget {
+  DiceRollerApp() : super();
 
-class _DiceRollerAppState extends State<DiceRollerApp> {
-  int diceNumber = 1;
-
-  void rollDice() {
-    setState(() {
-      diceNumber = Random().nextInt(6) + 1;
-    });
-  }
+  final Random random = Random();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Ludo'),
-          centerTitle: true,
-          backgroundColor: Colors.red,
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset('assets/dice_$diceNumber.png',
-                  width: 150, height: 150),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: rollDice,
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.yellow,
+    int diceNumber = 1;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ludo'),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: StatefulBuilder(
+          builder: (BuildContext context, StateSetter setState) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset('assets/dice_${diceNumber}.png',
+                    width: 150, height: 150),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      diceNumber = random.nextInt(6) + 1;
+                    });
+                  },
+                  style:
+                      ElevatedButton.styleFrom(backgroundColor: Colors.yellow),
+                  child: Text('Roll Dice'),
                 ),
-                child: Text('Roll Dice'),
-              ),
-            ],
-          ),
+              ],
+            );
+          },
         ),
       ),
     );
